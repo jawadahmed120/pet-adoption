b4edc75edbcbc6e714ba4eed26cc677add67bf20
Jawad Ahmed <jodye888@gmail.com>
2024-08-19 11:03:11 +0500

    Added fallback image

--- main.js
+++ main.js
@@ -19,8 +19,24 @@ async function petsArea(){
         const clone = template.content.cloneNode(true);
         console.log(pet.name);
         clone.querySelector("h3").textContent = pet.name;
+        clone.querySelector(".pet-description").textContent = pet.description;
+        if(!pet.photo) pet.photo = "images/Fallback.jpg";
+        clone.querySelector(".pet-card-photo img").src = pet.photo;
+        clone.querySelector(".pet-card-photo img").alt = `A ${pet.species} named ${pet.name}`;
+        clone.querySelector(".pet-age").textContent = createAgeText(pet.birthYear);
         wrapper.appendChild(clone);
     });
     document.querySelector(".list-of-pets").appendChild(wrapper);
 }
 petsArea();
+
+function createAgeText(birthYear){
+    const currentYear = new Date().getFullYear();
+    const age = currentYear - birthYear;
+
+    if(age == 0) return "1 year old";
+    if(age == 1) return "Less than a year old";
+
+    return `${age} years old`;
+
+}
